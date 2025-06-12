a9ee0046cb3481b042e2d343ec6b570a94e3f448
Vatsal Verma <127497796+vatsalver@users.noreply.github.com>
2025-06-12 14:35:20 +0530

    dom manipulation

--- js-projects/01_todo-localstorage-starter/01_todo-localstorage/script.js
+++ js-projects/01_todo-localstorage-starter/01_todo-localstorage/script.js
@@ -1,32 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let input = document.getElementById("todo-input");
-    let add = document.getElementById("add-task-btn");
-    let list = document.getElementById("todo-list")
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.getElementById("todo-input");
+  let add = document.getElementById("add-task-btn");
+  let list = document.getElementById("todo-list");
 
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    tasks.forEach(task => renderTasks(task));
+  tasks.forEach((task) => renderTasks(task));
 
-    add.addEventListener('click', () => {
-        let textinput = input.value.trim();
-        if (textinput === "") {
-            return
-        }
-        const newTask = {
-            id: Date.now(),
-            text: textinput,
-            completed: false,
-        }
-        tasks.push(newTask)
-        saveTasks();
-        input.value = ""
-        console.log(tasks)
-    })
-    function renderTasks(tasks) {
-        console.log(tasks);
+  add.addEventListener("click", () => {
+    let textinput = input.value.trim();
+    if (textinput === "") {
+      return;
     }
+    const newTask = {
+      id: Date.now(),
+      text: textinput,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks(newTask);
+    input.value = "";
+    console.log(tasks);
+  });
+  function renderTasks(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>`;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") {
+        return;
+      }
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
 
-    function saveTasks() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-})
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //prevent toggle event
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    list.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
